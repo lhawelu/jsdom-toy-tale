8d3933ad15cd487cab8161f0dc67077bf540097f
Larry Hawelu <lhawelu@gmail.com>
2021-03-03 11:28:25 -0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toyDataBase = 'http://localhost:3000/toys'
+  const getToys = (url) => {
+    fetch('http://localhost:3000/toys')
+    .then(r => r.json())
+    .then(toys => toys.forEach(toy => renderToys(toy)))
+  }
+  
+
+  getToys();
+  
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +24,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function renderToys(toy) {
+  console.log(toy)
+  const toyCollection = document.querySelector('#toy-collection')
+  const cardDiv = document.createElement('div')
+  cardDiv.innerHTML = 
+  `<div class="card"><h2>${toy.name}</h2><img src=${toy.image} class="toy-avatar" /><p>${toy.likes} </p><button class="like-btn">Like <</button></div>`
+  toyCollection.appendChild(cardDiv)
+}
